d3abd3138dc012e762d405b91166529c17bf9bab
강현국 <hyungook@ganghyeongug-ui-MacBookPro.local>
2024-04-02 14:39:08 +0900

    fix max length of model's response to 256

--- service/backend.py
+++ service/backend.py
@@ -36,7 +36,7 @@ def get_answer(prompt):
     inputs.to(device)
     
     # Generate
-    generate_ids = model.generate(inputs.input_ids, max_length=inputs.input_ids.shape[1]+5, 
+    generate_ids = model.generate(inputs.input_ids, max_length=256, 
                                   num_beams=1, no_repeat_ngram_size=3, early_stopping=True)
     
     output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
