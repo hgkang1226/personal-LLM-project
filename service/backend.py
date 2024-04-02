b5178e61732b1d9c15fdaf4aac05d23a6d41ab76
강현국 <hyungook@ganghyeongug-ui-MacBookPro.local>
2024-04-02 10:44:39 +0900

    fix device setting in backend

--- service/backend.py
+++ service/backend.py
@@ -17,17 +17,15 @@ model = LlamaForCausalLM.from_pretrained('h2oai/h2ogpt-4096-llama2-7b-chat')
 # h2oai/h2ogpt-4096-llama2-7b-chat
 # h2oai/h2ogpt-4096-llama2-13b-chat
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if torch.cuda.is_available():
-    device = torch.device('cuda')
     n_gpus = torch.cuda.device_count()
     if n_gpus > 1:
         model = torch.nn.DataParallel(model, device_ids=list(range(n_gpus)))
 
-else:
-    device = torch.device('cpu')
-
 model.to(device)
+
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
